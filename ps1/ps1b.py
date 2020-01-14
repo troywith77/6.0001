50bb7eb690cb548870c83db553e57225f0d7f33b
troywith77 <ruitang307@gmail.com>
2020-01-14 21:36:58 +0800

    ps1b

--- ps1/ps1b.py
+++ ps1/ps1b.py
@@ -4,6 +4,8 @@ annual_salary = float(input('The starting annual salary: '))
 portion_saved = float(input('The portion of salary to be saved: '))
 # 总房款
 total_cost = float(input('The cost of your dream home: '))
+# 半年涨薪比例
+semi_annual_raise = float(input('The semi­annual salary raise: '))
 # 月薪
 monthly_salary = annual_salary / 12
 # 首付比例
@@ -26,6 +28,12 @@ while (True):
     current_savings += current_savings * (r / 12)
     # 加上每个月新投资的钱
     current_savings += monthly_money
+
+    # 每6个月后涨薪
+    if (month % 6 == 0):
+        annual_salary *= (1 + semi_annual_raise)
+        monthly_salary = annual_salary / 12
+        monthly_money = monthly_salary * portion_saved
 
     if (current_savings >= down_payment):
         break
